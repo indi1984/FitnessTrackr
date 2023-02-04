aae14f65ea133f7c93c82a171d46bb828e14c5b9
Katherine Oelsner <octokatherine@github.com>
2023-02-04 11:39:07 -0800

    remove comments about implementation of attachActivitiesToRoutines

--- db/activities.js
+++ db/activities.js
@@ -14,10 +14,7 @@ async function getActivityById(id) {}
 async function getActivityByName(name) {}
 
 // used as a helper inside db/routines.js
-async function attachActivitiesToRoutines(routines) {
-  // select activities related to the routines
-  // attach array of activities to each routine object
-}
+async function attachActivitiesToRoutines(routines) {}
 
 async function updateActivity({ id, ...fields }) {
   // don't try to update the id
