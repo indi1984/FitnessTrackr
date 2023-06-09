fce8793f281b889d7587be1cca46ac8a088e357d
Kevin Harp <kharp1984@gmail.com>
2023-06-09 09:03:39 -0400

    all activities routes working

--- api/activities.js
+++ api/activities.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const activitiesRouter = express.Router();
 const { requireUser } = require('./utils');
-const { ActivityExistsError } = require('../errors');
+const { ActivityExistsError,
+  ActivityNotFoundError } = require('../errors');
+const { getPublicRoutinesByActivity } = require('../db/routines');
 const { getAllActivities,
   createActivity,
-  getActivityByName } = require('../db/activities');
+  updateActivity,
+  getActivityByName, 
+  getActivityById} = require('../db/activities');
 
 // GET /api/activities/:activityId/routines
+activitiesRouter.get('/:activityId/routines', async (req, res, next) => {
+  const activityId = req.params.activityId;
+  try {
+    const publicRoutines = await getPublicRoutinesByActivity({ id: activityId });
+    if (publicRoutines  && publicRoutines.length) {
+      res.send(publicRoutines);
+    } else {
+      throw ({
+        error: "Duplicates",
+        name: "Routines",
+        message: ActivityNotFoundError(activityId)
+      })
+    }
+  } catch (error) {
+    next(error);
+  } 
+});
 
 // GET /api/activities
 activitiesRouter.get('/', async (req, res, next) => {
@@ -24,12 +45,12 @@ activitiesRouter.post('/', requireUser, async (req, res, next) => {
     const existingActivity = await getActivityByName(request.name);
     if (!existingActivity) {
       const { name, description } = await createActivity(request);    
-      res.send({ description, name });
+      res.send({ name, description });
     } else {
       throw ({
         error: "Duplicates",
         name: "Activity",
-        message: ActivityExistsError(req.body.name),
+        message: ActivityExistsError(req.body.name)
       })
     }
   } catch (error) {
@@ -38,5 +59,35 @@ activitiesRouter.post('/', requireUser, async (req, res, next) => {
 });
 
 // PATCH /api/activities/:activityId
+activitiesRouter.patch('/:activityId', requireUser, async (req, res, next) => {
+  const { activityId }= req.params;
+  const { name, description }= req.body;
+  try {
+    const activityByName = await getActivityByName(name);
+    const activityById = await getActivityById(activityId);
+    if (activityByName) {
+      throw ({
+        error: "Duplicate",
+        name: "Activity Name",
+        message: ActivityExistsError(name)
+      })
+    } else if (!activityById) {
+      throw ({
+        error: "Does not exist",
+        name: "Activity Name",
+        message: ActivityNotFoundError(activityId)
+      })
+    } else {
+      const changedActivity = await updateActivity({
+         id: activityId, 
+         name, 
+         description 
+      });  
+      res.send(changedActivity);  
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = activitiesRouter;
